b413467e8f5e6b4021861a6d11dc68c07512140b
Name <168666798+pewpewgamer1@users.noreply.github.com>
2024-10-14 20:18:22 +0000

    Add files via upload

---
+++ builder.config.js
@@ -0,0 +1,17 @@
+options = {
+    username:           'Scylla',
+    avatar:             'https://images-ext-1.discordapp.net/external/B1h62T4uMfZLW9dc0_35fCUw7UGEyHbeWEpi2YdJ32U/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/1285954005687865394/da2d6d61656fb7d7a437ad47066d3c9b.webp?format=webp&width=676&height=676',
+    verified:           true,
+    noUser:             false,
+    dataSpecified:      false,
+    guiTabs:            ['author', 'description'],
+    useJsonEditor:      false,
+    reverseColumns:     false,
+    allowPlaceholders:  false,
+    autoUpdateURL:      false,
+    autoParams:         false,
+    hideEditor:         false,
+    hidePreview:        false,
+    hideMenu:           false,
+}
+onload = () => {}
